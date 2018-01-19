524bea67f04763c19a50a6cb2475dd4d5a980d83
CheeseNinja <wangd4@bxscience.edu>
2018-01-18 22:42:01 -0500

    Update Protanopia.java

--- Protanopia.java
+++ Protanopia.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Scanner;
 //First intesection- The circle has to match the corresponding color of the square
 //Second intersection- The circle has to go against the corresponding color of the square
 //More negative, circle is winning, more positive, square is winning
